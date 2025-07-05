8d6120da733a3544f8f86f55292a44e9bf14e845
sanji118 <sanjishifa32@gmail.com>
2025-07-05 13:15:27 +0600

    added faq controller

---
+++ controllers/faqController.js
@@ -0,0 +1,12 @@
+exports.getFaqs = async (req, res) => {
+  const collection = getCollection('faq');
+  const data = await collection.find().toArray();
+  res.send(data);
+};
+
+exports.getFaqById = async (req, res) => {
+  const id = req.params.id;
+  const collection = getCollection('faq');
+  const result = await collection.findOne({ _id: new ObjectId(id) });
+  res.send(result);
+};
