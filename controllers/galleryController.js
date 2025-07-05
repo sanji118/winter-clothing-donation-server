5ddc4f28a4bfeb1ad6ac5a59bec3075af786552e
sanji118 <sanjishifa32@gmail.com>
2025-07-05 13:14:20 +0600

    added gallery controller

---
+++ controllers/galleryController.js
@@ -0,0 +1,14 @@
+const { getCollection } = require("../utils/connectDB");
+
+exports.getGallery = async (req, res) => {
+  const collection = getCollection('gallery');
+  const data = await collection.find().toArray();
+  res.send(data);
+};
+
+exports.getGalleryItemById = async (req, res) => {
+  const id = req.params.id;
+  const collection = getCollection('gallery');
+  const result = await collection.findOne({ _id: new ObjectId(id) });
+  res.send(result);
+};
