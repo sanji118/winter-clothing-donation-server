b2df348c3d1c7ec4ef4bf1d161d819e1f8ff1030
sanji118 <sanjishifa32@gmail.com>
2025-07-07 14:48:37 +0600

    added post comment to blog"

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -13,3 +13,30 @@ exports.getBlogById = async (req, res) =>{
     const result = await collection.findOne({_id: new ObjectId(id) });
     res.send(result);
 }
+
+exports.postCommentToBlog = async (req, res) => {
+    try {
+        const collection = getCollection('blogs');
+        const id = req.params.id;
+        const {comment, user} = req.body;
+
+        const newComment = {
+            text: comment,
+            user,
+            date: new Date()
+        };
+
+        const result = await collection.updateOne(
+            {_id: new ObjectId(id)},
+            {$push: { comments : newComment }}
+        )
+
+        if (result.modifiedCount > 0) {
+            res.status(200).send({ success: true, message: "Comment added", comment: newComment });
+            } else {
+            res.status(404).send({ success: false, message: "Blog not found" });
+        }
+    } catch (error) {
+        res.status(500).send({ success: false, message: "Failed to add comment", error });
+    }
+}
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getBlogs, getBlogById } = require('../controllers/blogController');
+const { getBlogs, getBlogById, postCommentToBlog } = require('../controllers/blogController');
 const blogRoutes = express.Router();
 
 blogRoutes.get('/', getBlogs);
 blogRoutes.get('/:id', getBlogById);
+blogRoutes.post('/:id/comments', postCommentToBlog);
 
 module.exports = blogRoutes;
